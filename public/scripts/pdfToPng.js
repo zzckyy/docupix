391df112efd9223e51e5c729f72b3a4124b1d4ba
zzckyy <zackymm2080@gmail.com>
2026-06-08 22:22:00 +0700

    fix: remove file-saver dependency and clean up related code

--- public/scripts/pdfToPng.js
+++ public/scripts/pdfToPng.js
@@ -1,3 +1,6 @@
+// import '/pdfjs-dist'
+// import 'jszip';
+
 // setup pdf.js worker
 pdfjsLib.GlobalWorkerOptions.workerSrc =
   "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
